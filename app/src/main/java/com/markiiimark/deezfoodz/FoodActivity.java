82be588b8fbd8a5d62ad747180478fc33af3cc69
chanhi2000 <chanhi2000@gmail.com>
2017-05-07 01:37:16 +0900

    1st commit: successfully built gradle

---
+++ app/src/main/java/com/markiiimark/deezfoodz/FoodActivity.java
@@ -0,0 +1,13 @@
+package com.markiiimark.deezfoodz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FoodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food);
+    }
+}
