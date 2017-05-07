c22cdc255941f67ccf45711be5015d04bafdb348
chanhi2000 <chanhi2000@gmail.com>
2017-05-07 10:00:23 +0900

    v0.0.2: successfully made dagger appModule into application

--- app/src/main/java/com/markiiimark/deezfoodz/app/DeezFoodzApplication.java
+++ app/src/main/java/com/markiiimark/deezfoodz/app/DeezFoodzApplication.java
@@ -2,13 +2,26 @@ package com.markiiimark.deezfoodz.app;
 
 import android.app.Application;
 
+import com.markiiimark.deezfoodz.dagger.AppComponent;
+import com.markiiimark.deezfoodz.dagger.AppModule;
+import com.markiiimark.deezfoodz.dagger.DaggerAppComponent;
+
 /**
  * Created by MarkiiimarK on 5/7/17.
  */
 
 public class DeezFoodzApplication extends Application {
+    private AppComponent appComponent;
+    public AppComponent getAppComponent() {  return appComponent;  }
+
+    protected AppComponent initDagger(DeezFoodzApplication application) {
+        return DaggerAppComponent.builder()
+                .appModule(new AppModule(application))
+                .build();
+    }
 
     @Override public void onCreate() {
         super.onCreate();
+        appComponent = initDagger(this);
     }
 }
