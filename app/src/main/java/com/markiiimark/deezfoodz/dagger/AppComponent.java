fed0bd22388e4873f33003428bea3c6c6d96719f
chanhi2000 <chanhi2000@gmail.com>
2017-05-07 12:07:08 +0900

    v0.0.2: added NetworkModule into AppComponent and injected Food(z)PresenterImpl

--- app/src/main/java/com/markiiimark/deezfoodz/dagger/AppComponent.java
+++ app/src/main/java/com/markiiimark/deezfoodz/dagger/AppComponent.java
@@ -1,8 +1,9 @@
 package com.markiiimark.deezfoodz.dagger;
 
-import com.markiiimark.deezfoodz.app.DeezFoodzApplication;
 import com.markiiimark.deezfoodz.ui.food.FoodActivity;
 import com.markiiimark.deezfoodz.ui.foodz.FoodzActivity;
+import com.markiiimark.deezfoodz.ui.food.FoodPresenterImpl;
+import com.markiiimark.deezfoodz.ui.foodz.FoodzPresenterImpl;
 
 import javax.inject.Singleton;
 
@@ -12,8 +13,11 @@ import dagger.Component;
  * Created by MarkiiimarK on 5/7/17.
  */
 
-@Singleton @Component(modules = {AppModule.class, PresenterModule.class})
+@Singleton @Component(modules = {AppModule.class, PresenterModule.class, NetworkModule.class})
 public interface AppComponent {
     void inject(FoodzActivity target);
     void inject(FoodActivity target);
+
+    void inject(FoodzPresenterImpl target);
+    void inject(FoodPresenterImpl target);
 }
