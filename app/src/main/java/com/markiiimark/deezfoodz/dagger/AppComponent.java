3d7520e62906fc841ebc1c7d51ba078070fa3bb8
chanhi2000 <chanhi2000@gmail.com>
2017-05-07 11:36:26 +0900

    v0.0.2: add FoodPresenterImpl method

--- app/src/main/java/com/markiiimark/deezfoodz/dagger/AppComponent.java
+++ app/src/main/java/com/markiiimark/deezfoodz/dagger/AppComponent.java
@@ -1,6 +1,7 @@
 package com.markiiimark.deezfoodz.dagger;
 
 import com.markiiimark.deezfoodz.app.DeezFoodzApplication;
+import com.markiiimark.deezfoodz.ui.food.FoodActivity;
 import com.markiiimark.deezfoodz.ui.foodz.FoodzActivity;
 
 import javax.inject.Singleton;
@@ -14,4 +15,5 @@ import dagger.Component;
 @Singleton @Component(modules = {AppModule.class, PresenterModule.class})
 public interface AppComponent {
     void inject(FoodzActivity target);
+    void inject(FoodActivity target);
 }
